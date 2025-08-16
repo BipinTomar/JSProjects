bcdee54019068f6fd2336610d1e3d9ee2f7a698c
BipinTomar <bkumar_be16@thapar.edu>
2025-08-16 14:52:40 +0530

    BlogPost Changes added

---
+++ BlogPost/src/context/DataContext.js
@@ -0,0 +1,59 @@
+
+import api from '../api/posts';
+import useAxiosFetch from '../hooks/useAxiosFetch';
+import { createContext, useState, useEffect } from 'react';
+
+const DataContext = createContext({});
+
+export const DataProvider = ({ children }) => {
+    const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+
+    const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
+    
+    useEffect(() => {
+      setPosts(data);
+    }, [data])
+  
+    useEffect(() => {
+      const fetchData = async () =>{
+        try {
+          const response = await api.get('/posts');
+          setPosts(response.data);
+        } catch (err) {
+          if (err.response) {
+            // Not in the 200 response range 
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          } else {
+            console.log(`Error: ${err.message}`);
+          }
+        }
+      }
+      fetchData();
+  
+    },[]);
+  
+    useEffect(() => {
+      const filteredResults = posts.filter((post) =>
+        ((post.body).toLowerCase()).includes(search.toLowerCase())
+        || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+  
+      setSearchResults(filteredResults.reverse());
+    }, [posts, search]);
+
+    return (<DataContext.Provider value={{
+        search, setSearch,
+        searchResults, fetchError, isLoading,
+        posts, setPosts,
+
+    }}
+    >
+        {children}
+        </DataContext.Provider>
+        )
+}
+
+export default DataContext;
